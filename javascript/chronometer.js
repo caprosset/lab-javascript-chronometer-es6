1f4dc8c912f085a3539f845da9ff9be1284ae8c0
caprosset <capucinerosset@gmail.com>
2020-04-08 12:34:35 +0200

    Initial commit

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,65 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
+
+    // Bonus
+    this.currentMilliseconds = 0;
   }
-  startClick(callback) {
-    // ... your code goes here
+
+  startClick(printTime, printMilliseconds) {
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (printTime) printTime();
+    }, 1000);
+
+    // Bonus:
+    this.millisecondsIntervalId = setInterval( () => {
+      if (this.currentMilliseconds === 99) {
+        this.currentMilliseconds = 0;
+      }
+      this.currentMilliseconds += 1;
+      if (printMilliseconds) printMilliseconds();
+    }, 10);
   }
+
   getMinutes() {
-    // ... your code goes here
+    let currentTimeMin = Math.floor(this.currentTime / 60);
+    return currentTimeMin;
   }
+
   getSeconds() {
-    // ... your code goes here
+    let currentTimeSec = this.currentTime % 60;
+    return currentTimeSec;
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+
+  twoDigitsNumber(num) {
+    return ("0" + num).slice(-2);
   }
+
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
+
   resetClick() {
-    // ... your code goes here
+    this.currentTime = 0;
+    minDec.innerHTML = 0;
+    minUni.innerHTML = 0;
+    secDec.innerHTML = 0;
+    secUni.innerHTML = 0;
+
+    // Bonus:
+    this.currentMilliseconds = 0;
+    milDec.innerHTML = 0;
+    milUni.innerHTML = 0;
   }
+
   splitClick() {
-    // ... your code goes here
+    let minutes = this.twoDigitsNumber(this.getMinutes());
+    let seconds = this.twoDigitsNumber(this.getSeconds());
+    // Bonus:
+    let milliseconds = this.twoDigitsNumber(this.currentMilliseconds);
+
+    return `${minutes}:${seconds}:${milliseconds}`;
   }
 }
